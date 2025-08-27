9766ee31d10774d7ffa74b4bdd080baaf48062d3
Bazigar ( CFC ) <hdhindihub@gmail.com>
2025-08-27 07:25:01 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+API_ID = 1234567
+API_HASH = "YOUR_API_HASH"
+
+MONGO_URI = "mongodb+srv://username:password@cluster0.mongodb.net/?retryWrites=true&w=majority"
+DB_NAME = "gamebot"
