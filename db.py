388de3b967308604cf7d52a1b6cb3b0df67348d9
Bazigar ( CFC ) <hdhindihub@gmail.com>
2025-08-27 12:47:22 +0530

    Delete db.py

--- db.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-from config import MONGO_URI, DB_NAME
-
-client = MongoClient(MONGO_URI)
-db = client[DB_NAME]
-
-xo_collection = db["xo_games"]
-uno_collection = db["uno_games"]
